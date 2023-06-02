368150ac3a69ede8218514dc75a916631e8ba253
ProfeAlbeiro <profealbeiro2020@gmail.com>
2023-06-02 15:52:29 -0500

    Subiendo proyecto

--- controllers/Users.php
+++ controllers/Users.php
@@ -1,6 +1,18 @@
 <?php
     class Users{
         public function __construct(){}
-         
+        public function main(){
+            header("Location:?c=Dashboard");
+        }
+        public function createRol(){
+            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+                require_once "views/roles/admin/header.view.php";
+                echo "Estoy en el controlador CreateRol";
+                require_once "views/roles/admin/footer.view.php";
+            }
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                echo "Procesando la creación del Rol";
+            }
+        }
     }
 ?>
